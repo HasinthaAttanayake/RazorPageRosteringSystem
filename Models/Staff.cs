1ee97bf3adfc2a0f66431a635c92ff0889c838b4
Hasintha Attanayake <hasintha.attanayake@gmail.com>
2021-10-10 20:08:10 +1100

    updated display of staff fields and added Staff to navbar

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,18 @@ namespace RazorPageRosteringSystem.Models
 {
     public class Staff
     {
+        [Display(Name = "Staff ID")]
         public int staffID { get; set; }
-        public string name { get; set;} 
+        [Display(Name = "Full Name")]
+        public string name { get; set;}
+        [Display(Name = "Address")]
         public string address { get; set; }
+        [Display(Name = "Date Of Birth")]
         [DataType(DataType.Date)]
         public DateTime dateOfBirth { get; set; }
+        [Display(Name = "Email Address")]
         public string email { get; set; }
+        [Display(Name = "Mobile Number")]
         public string mob { get; set; }
 
     }
